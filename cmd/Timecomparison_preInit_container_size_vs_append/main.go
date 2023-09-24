9f55155fe3849397afa29b10a01e4d2736477b2f
singhxayush <fanduayush2000@gmail.com>
2023-09-25 01:11:17 +0530

    TIME COMPARISON OF INITIALIZATION BETWEEN PRE ALLOCATED AND APPEDED CONTAINER OF 1000000 SIZE

---
+++ cmd/Timecomparison_preInit_container_size_vs_append/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+
+	fmt.Println("");
+
+	var n int = 100000000
+	var test_slice1 = []int{}
+	var test_slice2 = make([]int, n)
+
+	fmt.Printf("Time without Pre allocation \t: %v\n", timeLoop(test_slice1, n))
+	fmt.Printf("Time with Pre allocation \t: %v\n\n", timeLoop(test_slice2, n))
+}
+
+func timeLoop(slice []int, n int) time.Duration {
+
+	var t_start = time.Now()
+
+	for len(slice) < n {
+		slice = append(slice, 1)
+	}
+
+	return time.Since(t_start)
+}
